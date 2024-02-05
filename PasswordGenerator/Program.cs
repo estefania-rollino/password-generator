58ea76f25f5a5e51d3d7997a558962362c767133
Steff Rollino <tefi.rollino@gmail.com>
2024-02-05 13:26:32 +0100

    PasswordGenerator v2. Include methods.

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -14,27 +14,40 @@ namespace PasswordGenerator
         {
             Console.WriteLine("Bienvenidos a PasswordGenerator!");
 
-            Console.WriteLine("¿Incluir Mayúsculas? (Y/n): ");
-            var includeCapitalLettersValue = Console.ReadLine()?.ToLower();
-            var includeCapitalLetters = string.IsNullOrEmpty(includeCapitalLettersValue) || includeCapitalLettersValue == "y" || includeCapitalLettersValue == "yes";
+            var includeCapitalLetters = GetUserChoice("¿Incluir Mayúsculas? (Y/n): ");
+            var includeNumbers = GetUserChoice("¿Incluir números? (Y/n): ");
+            var includeSpecialChars = GetUserChoice("¿Incluir caracteres especiales? (Y/n): ");
+            var passLength = RequestPasswordLength();
 
-            Console.WriteLine("¿Incluir números? (Y/n): ");
-            var includeNumbersValue = Console.ReadLine()?.ToLower();
-            var includeNumbers = string.IsNullOrEmpty(includeNumbersValue) || includeNumbersValue == "y" || includeNumbersValue == "yes";
+            var charSet = BuildPasswordCharSet(includeCapitalLetters, includeNumbers, includeSpecialChars);
+            var generatedPassword = GeneratePassword(passLength, charSet);
+            Console.WriteLine("Password: ");
+            Console.WriteLine(generatedPassword);
 
-            Console.WriteLine("¿Incluir caracteres especiales? (Y/n): ");
-            var includeSpecialCharsValue = Console.ReadLine()?.ToLower();
-            var includeSpecialChars = string.IsNullOrEmpty(includeSpecialCharsValue) || includeSpecialCharsValue == "y" || includeSpecialCharsValue == "yes";
+            Console.ReadKey();
+        }
 
-            var minLength = 4;
-            Console.WriteLine($"Ingresa la longitud de la contraseña (mínimo {minLength}): ");
+        private static bool GetUserChoice(string question)
+        {
+            Console.WriteLine(question);
+            var response = Console.ReadLine()?.ToLower();
+            return string.IsNullOrEmpty(response) || response == "y" || response == "yes";
+        }
+
+        private static int RequestPasswordLength() 
+        {
+            const int passwordMinLength = 4;
+            Console.WriteLine($"Ingresa la longitud de la contraseña (mínimo {passwordMinLength}): ");
             var input = Console.ReadLine();
             int.TryParse(input, out int passLength);
-            if (passLength < minLength)
-                passLength = minLength;
+            if (passLength < passwordMinLength)
+                passLength = passwordMinLength;
+            return passLength;
+        }
 
+        private static string BuildPasswordCharSet(bool includeCapitalLetters, bool includeNumbers, bool includeSpecialChars) 
+        {
             var charSet = new StringBuilder(lowercaseLetters);
-
             if (includeCapitalLetters)
                 charSet.Append(capitalLetters);
 
@@ -44,17 +57,19 @@ namespace PasswordGenerator
             if (includeSpecialChars)
                 charSet.Append(specialChars);
 
+            return charSet.ToString();
+        }
+
+        private static string GeneratePassword(int passwordLength, string charSet) 
+        {
             var random = new Random();
             var generatedPassword = "";
-            for (var i = 0; i < passLength; i++)
+            for (var i = 0; i < passwordLength; i++)
             {
                 var index = random.Next(charSet.Length);
                 generatedPassword += charSet[index];
             }
-            Console.WriteLine("Password: ");
-            Console.WriteLine(generatedPassword);
-
-            Console.ReadKey();
+            return generatedPassword;
         }
     }
 }
